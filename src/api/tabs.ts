5c78403b297cee85531c9ba754693adebf48c2de
mars <maspam@yandex.ru>
2022-11-09 22:33:17 +0300

    refactor: change class names

--- src/api/tabs.ts
+++ src/api/tabs.ts
@@ -21,8 +21,8 @@ export default class Tabs {
     this.tabsContainerElement?.classList.add('my-tabs');
     this.contentContainerElement?.classList.add('my-content');
 
-    this.tabElements[0].classList.add('active');
-    this.contentElements[0].classList.add('active');
+    this.tabElements[0].classList.add('my-tabs-active');
+    this.contentElements[0].classList.add('my-content-active');
 
     this.tabElements.forEach((elem, i) => {
       const handleTabClick = this.handleTabClick.bind(this, i);
@@ -33,19 +33,19 @@ export default class Tabs {
   }
 
   private handleTabClick(i: number) {
-    this.contentElements.forEach((elem) => elem.classList.remove('active'));
-    this.contentElements[i].classList.add('active');
+    this.contentElements.forEach((elem) => elem.classList.remove('my-content-active'));
+    this.contentElements[i].classList.add('my-content-active');
 
-    this.tabElements.forEach((elem) => elem.classList.remove('active'));
-    this.tabElements[i].classList.add('active');
+    this.tabElements.forEach((elem) => elem.classList.remove('my-tabs-active'));
+    this.tabElements[i].classList.add('my-tabs-active');
   }
 
   public destroy() {
     this.tabsContainerElement?.classList.remove('my-tabs');
     this.contentContainerElement?.classList.remove('my-content');
 
-    this.tabElements.forEach((elem) => elem.classList.remove('active'));
-    this.contentElements.forEach((elem) => elem.classList.remove('active'));
+    this.tabElements.forEach((elem) => elem.classList.remove('my-tabs-active'));
+    this.contentElements.forEach((elem) => elem.classList.remove('my-content-active'));
 
     this.tabElements.forEach((elem, i) => {
       elem.removeEventListener('click', this.callbacks[i]);
