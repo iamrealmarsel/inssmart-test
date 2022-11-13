0aefa21e2e2cf084d8b3ab7c12c1e002c5618c1d
mars <maspam@yandex.ru>
2022-11-14 00:17:39 +0300

    feat: program features button

--- src/components/bike/bike.ts
+++ src/components/bike/bike.ts
@@ -6,3 +6,47 @@ tabs.init();
 
 const accordion = new Accordion('#accordion', { breakpointMaxWidth: 600 });
 accordion.init();
+
+const formElements = document.querySelectorAll('[data-feature-add-to]');
+formElements.forEach((formElement) => {
+  formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (formElement instanceof HTMLFormElement) {
+      const formData = new FormData(formElement);
+      const feature = formData.get('feature');
+
+      if (typeof feature === 'string' && feature.trim() === '') {
+        formElement.classList.add('my-validation-error');
+        formElement.animate(
+          {
+            transform: [
+              'translateX(0)',
+              'translateX(-10px)',
+              'translateX(10px)',
+              'translateX(-10px)',
+              'translateX(8px)',
+              'translateX(-8px)',
+              'translateX(0)',
+            ],
+          },
+          {
+            duration: 500,
+          }
+        );
+
+        return;
+      }
+
+      const targetFeatureList = document.querySelector(`#${formElement.dataset.featureAddTo}`);
+      targetFeatureList?.insertAdjacentHTML(
+        'beforeend',
+        `<li class="bikes__feature">${feature}</li>`
+      );
+    }
+  });
+
+  formElement.addEventListener('input', () => {
+    formElement.classList.remove('my-validation-error');
+  });
+});
